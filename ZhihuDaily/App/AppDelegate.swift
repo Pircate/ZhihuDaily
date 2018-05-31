2fef36ff6738cf0642c30ef625cd6123838be84c
Pircate <gao497868860@163.com>
2018-05-31 19:04:22 +0800

    pod update RxNetwork

--- ZhihuDaily/App/AppDelegate.swift
+++ ZhihuDaily/App/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Network.shared.plugins = [NetworkIndicatorPlugin()]
+        Network.default.plugins = [NetworkIndicatorPlugin()]
         UIViewController.setupNavigationBar
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.white
